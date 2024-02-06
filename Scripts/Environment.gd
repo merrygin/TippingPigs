d7c03aa147a313137f014d73c4d1e10e9ae2ee23
merrygin <smitzjak@hu-berlin.de>
2024-02-06 14:01:33 +0100

    font fix

--- Scripts/Environment.gd
+++ Scripts/Environment.gd
@@ -10,8 +10,8 @@ func _ready():
 func _process(delta):
 	text = (
 		"Waldflächen: " + str(Game.forest_amount) + " ({0}%)".format([Game.forest_percent]) + 
-		"  |   Grassflächen: " + str(Game.grass_amount) + " ({0}%)".format([Game.grass_percent])  +
-		"  |   Zerstörtes Land: " + str(Game.deso_amount) + " ({0}%)".format([Game.deso_percent]) +
-		"  |   Durchschnitt: Unterholz - {0} Baumbestand - {1}".format([Game.average_underbrush, Game.average_tree_cover])
+		"\nGrassflächen: " + str(Game.grass_amount) + " ({0}%)".format([Game.grass_percent])  +
+		"\nZerstörtes Land: " + str(Game.deso_amount) + " ({0}%)".format([Game.deso_percent]) +
+		"\n\nDurchschnitt:\n   Unterholz - {0}\n   Baumbestand - {1}".format([Game.average_underbrush, Game.average_tree_cover])
 	)
 	
